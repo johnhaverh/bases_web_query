afacd2f46ab982129114a378ac0181dc5623f8c5
John Haver Herrera <johnhaverh@gmail.com>
2023-08-29 09:28:25 -0500

    add  handler for others routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:bases_web_query/ui/views/counter_provider_view.dart';
 import 'package:fluro/fluro.dart';
 import 'package:bases_web_query/ui/views/counter_view.dart';
 
@@ -7,15 +8,12 @@ class Flurorouter {
   static final FluroRouter router = new FluroRouter();
 
   static void configureRoutes(){
-    router.define(
-      '/',
-      handler: _counterHandler
-    );
+    router.define('/',handler: _counterHandler,transitionType: TransitionType.fadeIn);
+    router.define('/stateful',handler: _counterHandler,transitionType: TransitionType.fadeIn);
+    router.define('/provider',handler: _counterProviderHandler,transitionType: TransitionType.fadeIn);
   }
 
   //Handlers
-  static Handler _counterHandler = Handler(
-    handlerFunc: (context, params) => CounterView()
-  );
-
+  static Handler _counterHandler = Handler(handlerFunc: (context, params) => CounterView());
+  static Handler _counterProviderHandler = Handler(handlerFunc: (context, params) => CounterProviderView());
 }
